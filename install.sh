b287fb037cc8600b2211d530479ff0fa1f373d95
uvos <devnull@uvos.xyz>
2020-10-20 15:41:04 +0200

    disable pvrsrvinit in android to avoid pvr problems in mainline kernel

--- install.sh
+++ install.sh
@@ -39,6 +39,7 @@ adb push kexecboot /sdcard
 adb root/push busybox /sdcard
 adb shell "su -c 'mount -o remount,rw /system'"
 adb shell "su -c 'cp -r /sdcard/kexecboot /system/etc/'"
+adb shell "su -c 'cp /system/xbin/true /system/bin/pvrsrvinit'"
 adb shell "su -c 'mv  /system/bin/logwrapper /system/bin/logwrapper-backup'"
 adb shell "su -c 'cp /sdcard/kexebootstart.sh /system/bin/logwrapper'"
 adb shell "su -c 'cp /sdcard/busybox /system/bin/busybox'"
