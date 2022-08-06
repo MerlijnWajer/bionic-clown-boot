a40e1a6aa3422a52e731d4a34a4416cf03085361
Norayr Chilingarian <norayr@arnet.am>
2022-08-06 05:40:29 +0400

    fixes mount command fail.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo installing kexec to kexecboot
 adb push scripts/kexecbootstart.sh /sdcard
 adb push kexecboot /sdcard
 adb push root/busybox /sdcard
-adb shell "su -c 'mount -o remount,rw /system'"
+adb shell "su -c 'mount -o remount,rw /dev/block/system /system'"
 adb shell "su -c 'cp -r /sdcard/kexecboot /system/etc/'"
 adb shell "su -c 'cp /system/xbin/true /system/bin/pvrsrvinit'"
 adb shell "su -c 'mv  /system/bin/logwrapper /system/bin/logwrapper-backup'"
